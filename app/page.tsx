610e288c3dd4f688ad920bcec0883ab0394a8c03
namazihamed <h.namazi91@gmail.com>
2025-10-07 21:52:50 +0330

    style(header): created a separat component for main header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
+import Header from "@/components/header/Header";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import {
   ArrowRightIcon,
   FastForwardIcon,
-  LogInIcon,
   LucideToolCase,
   SearchCheckIcon,
-  UserPlus2,
 } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 const features = [
@@ -24,7 +24,7 @@ const features = [
     content:
       "Using out third party login system, you can register and login on the go",
     Icon: FastForwardIcon,
-    iconColor: "text-violet-300",
+    iconColor: "text-red-500",
   },
 
   {
@@ -33,7 +33,7 @@ const features = [
     content:
       "No extra tools, just the things you need. headings, text alignment, lists, color and highlight changer",
     Icon: LucideToolCase,
-    iconColor: "text-sky-500",
+    iconColor: "text-violet-300",
   },
 ];
 
@@ -41,41 +41,22 @@ export default function Home() {
   return (
     <div className="min-w-screen min-h-screen relative">
       <div className="absolute top-0 left-0 inset-0 bg-[url('/banner.png')] bg-cover bg-center z-0 opacity-40"></div>
-      <header
-        className="absolute top-0 left-0 w-full h-20 bg-card z-50 flex items-center justify-between
-      p-3"
-      >
-        <div></div>
-        <div className="space-x-2">
-          <Button asChild size={"sm"}>
-            <Link href={"/login"}>
-              <LogInIcon /> Login
-            </Link>
-          </Button>
-
-          <Button asChild size={"sm"}>
-            <Link href={"/register"}>
-              <UserPlus2 /> Signup
-            </Link>
-          </Button>
-        </div>
-      </header>
+      <Header />
       <div className=" pt-20 absolute min-w-screen min-h-screen flex flex-col justify-around items-center">
         <div
           className="
           flex items-center justify-center flex-col text-white z-40 space-y-5"
         >
-          <h2 className="font-bold text-pretty text-primary text-3xl md:text-4xl lg:text-5xl">
+          <h2 className="font-bold text-pretty text-primary text-4xl md:text-5xl lg:text-6xl">
             Your second brain is here!
           </h2>
-          <p className="animate-in slide-in-from-bottom-120 duration-700 fade-in-10">
-            Write down your thoughts and get them out of your head!
-          </p>
+          <p>Write down your thoughts, get them out of your head!</p>
           <Button
             asChild
             size={"lg"}
             className="group hover:-translate-y-0.5 
-          hover:scale-105 transition-all duration-500"
+              animate-in slide-in-from-bottom-80 duration-300 fade-in-0
+              scale-110 transition-all "
           >
             <Link href={"/register"}>
               <ArrowRightIcon className="group-hover:animate-caret-blink" /> Get
@@ -85,17 +66,19 @@ export default function Home() {
         </div>
 
         <div className="grid grid-cols-1  md:grid-cols-3 gap-2 md:gap-5 py-10 max-w-3xl">
-          {features.map(({ id, title, content, Icon }) => (
+          {features.map(({ id, title, content, Icon, iconColor }) => (
             <Card
               key={id}
-              className="hover:scale-105 hover:-translate-y-0.5 duration-300 transition-all"
+              className="shadow-none hover:scale-105 hover:-translate-y-0.5 duration-300 transition-all"
             >
               <CardContent>
-                <CardTitle className="font-semibold italic text-primary mb-3 text-center">
+                <CardTitle className="font-semibold italic mb-3 text-center">
                   {title}
                 </CardTitle>
-                <Icon className="w-5 h-5 mx-auto" />
-                <p className="text-xs text-center text-pretty">{content}</p>
+                <Icon className={`w-5 h-5 mx-auto ${iconColor} mb-1`} />
+                <p className="text-xs text-pretty text-muted-foreground">
+                  {content}
+                </p>
               </CardContent>
             </Card>
           ))}
---
+++ components/header/Header.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import { LogInIcon, UserPlus2 } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header
+      className="absolute top-0 left-0 w-full h-20 bg-card z-50 flex items-center justify-between
+      p-3"
+    >
+      <div>
+        <Link href={".."}>
+          <Image src={"/logo.png"} width={120} height={50} alt="Parch logo" />
+        </Link>
+      </div>
+      <div className="space-x-2">
+        <Button asChild size={"sm"} variant={"outline"}>
+          <Link href={"/login"}>
+            <LogInIcon /> Login
+          </Link>
+        </Button>
+
+        <Button asChild size={"sm"} variant={"outline"}>
+          <Link href={"/register"}>
+            <UserPlus2 /> Signup
+          </Link>
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
